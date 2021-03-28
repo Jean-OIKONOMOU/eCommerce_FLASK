e6eb00c1eb6ef69d5047a72ef34b404ffd69e96f
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-03-29 00:27:19 +0200

    created USER db model and foreign key link for a future BAsket functionality

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # the app is this file
@@ -9,12 +8,7 @@ app = Flask(__name__)
 # this is the config for the sqlite3 db I created
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 
-# the db is linked to this file
+# thid line links the db to this file
 db = SQLAlchemy(app)
 
-# here are the routes of the web page
 from market import routes
-
-# export FLASK_APP=market.py
-# export FLASK_DEBUG=1
-# flask run
--- market/models.py
+++ market/models.py
@@ -1,7 +1,25 @@
 from market import db
 
 
-# WARNING : after creating your model you should go in a Python shell and type 'from market import db' then 'db.create_all()'. Market being the value of __name__. This creates an SQL db.
+# WARNING : after creating your model you should go in a Python shell and type
+# 'from market import db' then 'db.create_all()'.
+# Market being the value of __name__. This creates an SQL db.
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50),
+                              nullable=False,
+                              unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+
+# backref='owned_user'
+# Used for tracing back the owner of the item that is being purchased
+# since we'll be making a link between a User item and an Item item.
+# lazy=True
+# tells SQLAlchemy to grab all the items in one go.
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -10,6 +28,7 @@ class Item(db.Model):
     description = db.Column(db.String(length=1024),
                             nullable=False,
                             unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
