10fb76c7d6bb30d1dd22b689d002bbf076357b8e
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2021-04-04 23:45:38 +0200

    implemented logout button

--- market/models.py
+++ market/models.py
@@ -22,6 +22,14 @@ class User(db.Model, UserMixin):
     budget = db.Column(db.Integer(), nullable=False, default=1000)
     items = db.relationship('Item', backref='owned_user', lazy=True)
 
+    # this shit doesn't work !
+    @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
+
     @property
     def password(self):
         return self.password
